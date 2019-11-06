beac4a3b1b813bc0afd3a3fb134efb1f30871a83
drunkerbull <mvgamesdw@gmail.com>
2019-11-06 22:17:34 +0100

    some update search 4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   host: 'https://mtapp-timer.herokuapp.com/api',
-  ws: 'ws://mtapp-timer.herokuapp.com'
+  ws: 'https://mtapp-timer.herokuapp.com'
 };
